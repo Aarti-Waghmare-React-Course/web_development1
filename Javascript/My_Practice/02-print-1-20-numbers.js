4ec075611ce94f1b591fbbd9491e43ffcb3e5228
Aarti Waghmare <aarticeo5@gmail.com>
2026-03-06 15:01:00 +0530

    print-1-20-numbers

---
+++ Javascript/My_Practice/02-print-1-20-numbers.js
@@ -0,0 +1,12 @@
+//Write a program to print numbers from 1 to 20
+
+let num = 20;
+let result = " ";
+
+for(let i=1; i<=num;i++){
+    result = result + i +" ";
+} 
+console.log(result);
+
+// o/p ---> 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20
+// " " ---> for horizontal result
