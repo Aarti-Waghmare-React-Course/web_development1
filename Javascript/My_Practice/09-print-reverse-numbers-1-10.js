bcc6665ec63e9c7cbfa2abc71c5ea910160d3b1a
Aarti Waghmare <aarticeo5@gmail.com>
2026-03-06 22:18:51 +0530

    print-reverse-numbers

---
+++ Javascript/My_Practice/09-print-reverse-numbers-1-10.js
@@ -0,0 +1,9 @@
+// Write a program to Print numbers from 10 to 1 (reverse order)
+
+let num = 10;
+let result = "";
+
+for(let i=10;i>=1;i--){
+    result = result + i + " ";
+}
+console.log(result);
